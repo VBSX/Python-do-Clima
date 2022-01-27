dc17071adbd1f9508744dbdad8f6430217d6a0c6
Vitor <84788940+VBSX@users.noreply.github.com>
2022-01-26 22:56:37 -0300

    Python clima altualizado para 1.3, agora com uma interface grafica

--- Tempo.py
+++ Tempo.py
@@ -1,25 +1,56 @@
 import requests
+from tkinter import *
 
 API_KEY = "106dc89a610c7f04f0ebbd6186596a19"
 base_url = "http://api.openweathermap.org/data/2.5/weather"
 #http://api.openweathermap.org/data/2.5/weather?id=524901&appid={API_KEY}&lang={pt_br}#
 
-city = input("Coloque a cidade desejada: ")
-request_url = f"{base_url}?appid={API_KEY}&q={city}&lang=pt_br"
-response = requests.get(request_url)
-celcius = - 273.15 , 2
-if response.status_code == 200:
-    data = response.json()
-    weather = data['weather'][0]['description']
-    temperatura = round(data["main"]["temp"] - 273.15 , 2)
-    sensação_termica = round(data["main"]['feels_like'] - 273.15 , 2)
-    temperatura_max = round(data["main"]['temp_max'] - 273.15 , 2)
     
-    #print(data)
-    print("O clima está", weather)
-    print("Temperatura em ",city,"é de", temperatura,"°Celsius",)
-    print("Com sensação térmica de",sensação_termica,"°Celsius")
-    #print("A temperatura máxima hoje foi de",temperatura_max,"°Celsius")
-else:
-    print("Algo de errado não está certo! Tente novamente")
-    
+def clima_python():
+    #city = input("Coloque a cidade desejada: ")
+    #city = "maringa"
+    request_url = f"{base_url}?appid={API_KEY}&q={city}&lang=pt_br"
+    response = requests.get(request_url)
+    #celcius = - 273.15 , 2
+    if response.status_code == 200:
+        data = response.json()
+        weather = data['weather'][0]['description']
+        temperatura = round(data["main"]["temp"] - 273.15 , 2)
+        sensação_termica = round(data["main"]['feels_like'] - 273.15 , 2)
+        temperatura_max = round(data["main"]['temp_max'] - 273.15 , 2)
+        #print(data)
+        #print("O clima está", weather)
+        #print("Temperatura em ",city,"é de", temperatura,"°Celsius",)
+        #print("Com sensação térmica de",sensação_termica,"°Celsius")
+        #print("A temperatura máxima hoje foi de",temperatura_max,"°Celsius")
+        clim =  f'''
+       
+        O clima está {weather}
+        Temperatura em {city} é de {temperatura} °Celsius
+        Com sensação térmica de {sensação_termica}°Celsius)
+        '''
+        texto_clima["text"]= clim
+    else:
+        print("Algo de errado não está certo! Tente novamente")
+
+janela = Tk()
+janela.title("Python Clima")
+#janela.geometry("300x300")
+
+texto_orientação = Label(janela, text="Seja Bem vindo Ao Python Clima!")
+texto_orientação.grid(column=0, row=0, padx=10, pady=20)
+
+texto_cidade = Label(janela, text="Insira aqui a cidade desejada",)
+texto_cidade.grid(column=0, row=1, padx=10, pady=20)
+city=Entry(janela)
+city.place(x=43, y=55)
+
+
+botao = Button(janela, text="Iniciar", command=clima_python)
+botao.grid(column=0, row=2, padx=10, pady=10)
+
+texto_clima = Label(janela, text="")
+texto_clima.grid(column=0, row=3, padx=10, pady=10)
+
+
+janela.mainloop()
