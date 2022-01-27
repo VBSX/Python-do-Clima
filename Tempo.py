3218dc29a86fdf92dd394a79d579492894a712ae
Vitor Borges <vitorgamer.mais@gmail.com>
2022-01-26 21:40:59 -0300

    Python clima atualizado para a versão 1.2

--- Tempo.py
+++ Tempo.py
@@ -7,13 +7,19 @@ base_url = "http://api.openweathermap.org/data/2.5/weather"
 city = input("Coloque a cidade desejada: ")
 request_url = f"{base_url}?appid={API_KEY}&q={city}&lang=pt_br"
 response = requests.get(request_url)
-
+celcius = - 273.15 , 2
 if response.status_code == 200:
     data = response.json()
     weather = data['weather'][0]['description']
-    temperature = round(data["main"]["temp"] - 273.15 , 2)
-
-    print("Clima: ", weather)
-    print("Temperatura: ", temperature, "Celsius")
+    temperatura = round(data["main"]["temp"] - 273.15 , 2)
+    sensação_termica = round(data["main"]['feels_like'] - 273.15 , 2)
+    temperatura_max = round(data["main"]['temp_max'] - 273.15 , 2)
+    
+    #print(data)
+    print("O clima está", weather)
+    print("Temperatura em ",city,"é de", temperatura,"°Celsius",)
+    print("Com sensação térmica de",sensação_termica,"°Celsius")
+    #print("A temperatura máxima hoje foi de",temperatura_max,"°Celsius")
 else:
-    print("Algo de errado não está certo!")
+    print("Algo de errado não está certo! Tente novamente")
+    
