754bd8e50a78e6336e0d334ae39d3392809f74e1
Vitor <84788940+VBSX@users.noreply.github.com>
2022-01-28 11:18:52 -0300

    Python Do Clima-2.0
    Interface Gráfica agora é funcional!

--- Python Do Clima.py
+++ Python Do Clima.py
@@ -9,20 +9,22 @@ from tkinter import *
 
 load_dotenv()
 janela = Tk()
-
-API_KEY = os.getenv("API_KEY")
-
 base_url = "http://api.openweathermap.org/data/2.5/weather"
 #http://api.openweathermap.org/data/2.5/weather?id=524901&appid={API_KEY}&lang={pt_br}#
 
+API_KEY = os.getenv("API_KEY")
+"""def salvar_dados():
+    global city
+
+    city = city.get()
+"""
 def clima_python():
+    global city
+    city = city_janela.get()
     #city = input("Coloque a cidade desejada: ")
     request_url = f"{base_url}?appid={API_KEY}&q={city}&lang=pt_br"
     response = requests.get(request_url)
-  
     #celcius = - 273.15 , 2
-    Linha_Entrey_city = city
-
     if response.status_code == 200:
         #janela_dados = Tk()
         data = response.json()
@@ -31,7 +33,6 @@ def clima_python():
         sensação_termica = round(data["main"]['feels_like'] - 273.15 , 2)
         temperatura_max = round(data["main"]['temp_max'] - 273.15 , 2)
         clim =  f'''
-       
         O clima está {weather}
         Temperatura em {city} é de {temperatura}°Celsius
         Com sensação térmica de {sensação_termica}°Celsius)
@@ -40,7 +41,6 @@ def clima_python():
         
     else:
         erro = f'''
-       
         Favor Tente novamente 
         inserindo a cidade desejada
         '''
@@ -49,21 +49,25 @@ def clima_python():
     
 janela.title("Python Clima")
 #janela.geometry("300x300")
-load = Image.open("imagens\python do clima.png")
-render = ImageTk.PhotoImage(load)
+
+img = PhotoImage(file="Imagens/Python do clima2.png")
+label_imagem = Label(janela, image=img)
+label_imagem.grid()
 
 texto_orientação = Label(janela, text="Seja Bem vindo Ao Python Clima!")
-texto_orientação.grid(column=0, row=0, padx=10, pady=20)
+texto_orientação.place(x=384)
 
 texto_cidade = Label(janela, text="Insira aqui a cidade desejada", )
-texto_cidade.grid(column=0, row=1, padx=10, pady=20)
-city=Entry(janela)
-city.place(x=43, y=55)
+texto_cidade.place(x=390, y=50)
+
+city_janela=Entry(janela)
+city_janela.place(x=405, y=80)
 
 botao = Button(janela, text="Descobrir o clima", command=clima_python)
-botao.grid(column=0, row=2, padx=10, pady=10)
+botao.place(x=415, y=120)
 
 texto_clima = Label(janela, text="")
-texto_clima.grid(column=0, row=3, padx=10, pady=10)
+texto_clima.place(x=350, y=300)
+texto_clima.grid(pady=10)
 
 janela.mainloop()
